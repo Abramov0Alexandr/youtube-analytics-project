0008e933a7a4d0e34691a2cdfea4ec8b02d70e2b
Alexandr Abramov <alexandr.abramovvv@yandex.ru>
2023-03-31 16:20:47 +0300

    Изменил режим доступа к атрибуту playlist_ids и сделал геттер

--- src/PLVideo.py
+++ src/PLVideo.py
@@ -5,4 +5,8 @@ class PLVideo(Video):
 
     def __init__(self, video_id: str, playlist_ids: str):
         super().__init__(video_id)
-        self.playlist_ids = playlist_ids
+        self.__playlist_ids = playlist_ids
+
+    @property
+    def playlist_ids(self):
+        return self.__playlist_ids
