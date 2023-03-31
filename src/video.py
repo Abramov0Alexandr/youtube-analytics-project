db1aba6b4691f5752cbe8daa44be91e7fdd3d5e3
Alexandr Abramov <alexandr.abramovvv@yandex.ru>
2023-03-31 16:15:37 +0300

    Написал дополнительный метод для проверки существования видео

--- src/video.py
+++ src/video.py
@@ -1,5 +1,6 @@
 import os
 from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 
 
 class Video:
@@ -9,23 +10,35 @@ class Video:
 
     def __init__(self, video_id: str):
         self.__video_id = video_id
-        self.__video_response = self.__youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
-                                                             id=self.__video_id).execute()
-        self.__video_title = self.__video_response['items'][0]['snippet']['title']
-        self.__video_url = f"https://www.youtube.com/watch?v={self.__video_id}"
-        self.__view_count = self.__video_response['items'][0]['statistics']['viewCount']
-        self.__like_count = self.__video_response['items'][0]['statistics']['likeCount']
+        if self.__verify_video_id(video_id) != 0:
+
+            self.__video_response = self.__youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                                                 id=self.__video_id).execute()
+            self.__title = self.__video_response['items'][0]['snippet']['title']
+            self.__video_url = f"https://www.youtube.com/watch?v={self.__video_id}"
+            self.__view_count = self.__video_response['items'][0]['statistics']['viewCount']
+            self.__like_count = self.__video_response['items'][0]['statistics']['likeCount']
+        else:
+            self.__title = None
+            self.__video_url = None
+            self.__view_count = None
+            self.__like_count = None
 
     def __str__(self) -> str:
-        return self.__video_title
+        return self.__title
+
+    def __verify_video_id(self, video_id):
+        video_response = self.__youtube.videos().list(part='status', id=video_id).execute()
+        search_count = video_response['pageInfo']['totalResults']
+        return search_count
 
     @property
     def video_id(self) -> str:
         return self.__video_id
 
     @property
-    def video_title(self) -> str:
-        return self.__video_title
+    def title(self) -> str:
+        return self.__title
 
     @property
     def video_url(self) -> str:
