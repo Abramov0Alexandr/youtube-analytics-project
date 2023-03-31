66fcd0d35a267f19c481cf487a6c2b4d8240580b
Alexandr Abramov <alexandr.abramovvv@yandex.ru>
2023-03-31 16:24:41 +0300

    Добавил пример работы при корректном id видео

--- homework-6/main.py
+++ homework-6/main.py
@@ -4,3 +4,12 @@ if __name__ == '__main__':
     broken_video = Video('broken_video_id')
     assert broken_video.title is None
     assert broken_video.like_count is None
+
+
+# Пример работы программы в случае, если id видео передано корректно
+    video1 = Video('9lO06Zxhu88')  # '9lO06Zxhu88' - это id видео из ютуб
+    assert str(video1.video_url)
+    assert str(video1.video_id)
+    assert str(video1.title)
+    assert str(video1.like_count)
+    assert str(video1.view_count)
